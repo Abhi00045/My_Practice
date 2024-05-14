aa7b68144e354337c261141eef4623dce2110c0f
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-05-14 20:29:15 +0530

    ready

--- leetcode/leetcode2.js
+++ leetcode/leetcode2.js
@@ -11,3 +11,28 @@ let orginalString =`GEEKSTER`
 let op =stringRev(orginalString)
 console.log(orginalString);
 console.log(op);
+
+
+// Anagram Check: 
+// Implement an algorithm to check if two strings are anagrams of each other (contain the same characters with the same frequency)
+
+function anagram(s,t){
+    if(s.length!==t.length){
+        return false;
+    }
+    const map1={};
+    const map2={};
+    for(let i=0;i<s.length;i++){
+        let ch1=s[i]
+        map1[ch1]=(map1[ch1]||0)+1;
+        let ch2=t[i]
+        map2[ch2]=(map2[ch2]||0)+1;
+    }
+    for(let i=0;i<s.length;i++){
+        if(map1[s[i]]!=map2[s[i]]){
+            return false
+        }return true
+    }
+}
+console.log(anagram("anagram","nagaram"));
+console.log(anagram("abhinav","reddy"));
