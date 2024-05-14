3fd63c1e8484444aa23780a6566b29bc1746b556
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-05-14 23:29:04 +0530

    done for a day

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let n=10
-for(i=1;i<n;i++){
-    for(j=1;j<n;j++){
-        console.log(j+" ");
-    }
-    console.log();
-}
+// let n=10
+// for(i=1;i<n;i++){
+//     for(j=1;j<n;j++){
+//         console.log(j+" ");
+//     }
+//     console.log();
+// }
 // function fun(x){
 //     function inner(y) {
 //         return x+y;
@@ -17,15 +17,33 @@ for(i=1;i<n;i++){
     
 // }
 
-let players=[
-    {name:"abhi",score:200},
-    {name:"akhil",score:200},
-    {name:"abhinav",score:200},
-    {name:"saritha",score:200},
-    {name:"kohli",score:200},
-    {name:"dhoni",score:200}
-]
-console.log(players.push(
-    {name:"pandya" , score:4}
-));
+// let players=[
+//     {name:"abhi",score:200},
+//     {name:"akhil",score:200},
+//     {name:"abhinav",score:200},
+//     {name:"saritha",score:200},
+//     {name:"kohli",score:200},
+//     {name:"dhoni",score:200}
+// ]
+// console.log(players.push(
+//     {name:"pandya" , score:4}
+// ));
 
+// function secondLar(arr){
+//     let num = arr[0];
+//     for(i=0;i<arr.length;i++){
+//         if(num<arr[i]){
+//             num = arr[i];
+//             return num;
+//         }
+//     }return num;
+// }
+// console.log(secondLar([1,2,3,4]));
+
+// function name(sum) {
+//    return sum(1,2);
+// }
+// function sum(a,b) {
+//     return a+b;  
+// }
+// console.log(name(sum));
