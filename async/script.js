01484e0477d66b701c44a12344d28901534ee61e
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-06-10 02:42:21 +0530

    changed

---
+++ async/script.js
@@ -0,0 +1,33 @@
+let order = document.getElementById("order");
+
+function selectPizza(callback){
+    setTimeout(()=>{
+        let pizza = "chicken";
+    console.log(`step1:${pizza}`);
+    callback(pizza);
+    },1000)
+}
+function orderPizza(pizza,callback){
+setTimeout(()=>{
+    let pizzaOrder = "orderPlaced";
+    console.log(`step 2:${pizzaOrder}`);
+    callback(pizzaOrder,pizza);
+},2000)
+
+}
+
+function orderPlaced(pizzaOrder,pizza){
+setTimeout(()=>{
+    console.log(`step3:${pizzaOrder}`);
+    console.log(`enjoy mam ${pizza}`);
+},2300)
+}
+
+order.addEventListener("click",()=>{
+    console.log(`processing`);
+    selectPizza((pizza)=>{
+        orderPizza(pizza,(pizzaOrder,pizza)=>{
+            orderPlaced(pizzaOrder,pizza);
+        })
+    })
+})
